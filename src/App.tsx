74136eae99f3ac9b44eb43a9404e436110bf07a0
codearchitect99 <ysw991106@gmail.com>
2025-03-22 06:53:32 +0900

    update'

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Link, Route, HashRouter as Router, Routes} from "react-router-dom";
 import {Resume} from "./pages/resume/Resume.tsx";
 import { Home } from "./pages/home/Home.tsx";
 import { NotFound } from "./pages/notFound/NotFound.tsx";
+import { CoverLetter } from "./pages/coverletter/CoverLetter.tsx";
 
 const App: React.FC = () => {
   return (
@@ -21,12 +22,19 @@ const App: React.FC = () => {
           >
             Resume
           </Link>
+          <Link
+            to="/coverletter"
+            className="px-3 py-2 rounded text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+          >
+            Cover letter
+          </Link>
         </div>
       </nav>
       
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="/coverletter" element={<CoverLetter />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
