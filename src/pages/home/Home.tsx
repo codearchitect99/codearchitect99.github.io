5366f78cd635b85a8afaf12bccdfe54a73c1405e
codearchitect99 <ysw991106@gmail.com>
2025-03-23 16:49:10 +0900

    update layout pofil

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -50,30 +50,50 @@ export const Home = () => {
 	
 	return (
 		<main className="flex flex-col items-center space-y-8">
-			<section
-				id="about"
-				className="w-full h-[10vh] flex items-center justify-center bg-gradient-to-b from-white via-blue-50 to-blue-300"
+			<section id="header"
+			         className="w-full h-[20vh] flex items-center justify-center bg-gradient-to-b from-white via-blue-50 to-blue-300"
 			>
-				<div className="max-w-4xl text-center px-4">
-					<h1 className="text-5xl md:text-6xl font-extrabold mb-6 leading-tight text-gray-900 tracking-tight">
-      <span className="bg-gradient-to-r from-sky-500 to-blue-700 bg-clip-text text-transparent">
-       Seongwon Yang
-      </span>
-					</h1>
+				<div className="flex flex-col-reverse md:flex-row-reverse items-center justify-center gap-4 w-full max-w-4xl px-4">
+					<section id="contacts" className="text-base text-gray-700 md:ml-4 flex flex-col">
+						<div className="flex justify-start gap-1">
+							<HiOutlineMail className="text-xl text-blue-600" />
+							<a href="mailto:ysw991106@gmail.com" className="hover:underline hover:text-blue-800 transition">
+								ysw991106@gmail.com
+							</a>
+						</div>
+						<div className="flex justify-start gap-1">
+							<p className="flex items-center justify-center gap-2">
+								<GoLocation className="text-xl text-red-500" />
+								Jeju, South Korea
+							</p>
+						</div>
+						<div className="flex justify-start gap-1">
+							<p className="flex items-center justify-center gap-2">
+								<FaGithub className="text-xl text-gray-800 dark:text-white" />
+								<a href="https://github.com/codearchitect99" target="_blank" rel="noopener noreferrer"
+								   className="hover:underline hover:text-blue-800 transition">
+									GitHub
+								</a>
+							</p>
+						</div>
+					</section>
 					
-					{/*<p className="text-gray-800 text-lg md:text-xl leading-relaxed mb-10 drop-shadow-sm">*/}
-					{/*	복잡한 시스템을 구조적으로 이해하고 흐름을 정리해 나가는 것을 좋아합니다.<br/>*/}
-					{/*	작은 기능에도 설계의 의도를 담아, 유지보수성과 확장성을 고려한 개발을 지향합니다.*/}
-					{/*</p>*/}
+					<section id="name" className="text-center">
+						<h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-gray-900 tracking-tight">
+							<p className="bg-gradient-to-r from-sky-500 to-blue-700 bg-clip-text text-transparent">
+								Seongwon Yang
+							</p>
+						</h1>
+					</section>
 				</div>
 			</section>
 			
-			<section id="about" className="w-full max-w-3xl border-b border-gray-300 pb-8">
+			<section id="about" className="w-full max-w-3xl border-b border-gray-300 pb-8 px-4">
 				<h2 className="text-2xl font-semibold mb-4">About Me</h2>
 				<p className="text-gray-700 leading-relaxed">
 					복잡한 시스템을 구조적으로 이해하고, 흐름을 정리해 나가는 과정에 흥미를 느끼는 개발자입니다.
 					작은 기능 하나에도 설계의 의도를 담아 신중하게 접근하며, 유지보수성과 확장성을 고려한 안정적인 개발을 지향합니다.
-					<br /><br />
+					<br/><br/>
 					무엇이 왜 그렇게 동작하는지를 깊이 이해하려고 노력하며, 단순한 구현을 넘어서 원리를 탐구하는 과정을 즐깁니다.
 					아직 배워야 할 부분이 많지만, 책임감 있는 태도로 팀에 기여하며 함께 성장할 수 있는 개발자가 되고자 합니다.
 					변화에 유연하게 대응하되, 기본에 충실한 개발자가 되는 것을 목표로 하고 있습니다.
@@ -81,7 +101,7 @@ export const Home = () => {
 				</p>
 			</section>
 			
-			<section id="skills" className="w-full max-w-3xl border-b border-gray-300 pb-8">
+			<section id="skills" className="w-full max-w-3xl border-b border-gray-300 pb-8 px-4">
 				<h2 className="text-2xl font-semibold mb-4">Skills</h2>
 				<ul className="list-disc pl-5 space-y-1 text-gray-700">
 					<li>
@@ -100,12 +120,13 @@ export const Home = () => {
 						<strong>Testing</strong>: JUnit
 					</li>
 					<li>
-						<strong>Tools & Collaboration</strong>: Git, GitHub, GitHub Projects, GitHub Docs, Figma, Jira, Confluence, Agit
+						<strong>Tools & Collaboration</strong>: Git, GitHub, GitHub Projects, GitHub Docs, Figma, Jira, Confluence,
+						Agit
 					</li>
 				</ul>
 			</section>
 			
-			<section id="projects" className="w-full max-w-3xl border-b border-gray-300 pb-8">
+			<section id="projects" className="w-full max-w-3xl border-b border-gray-300 pb-8 px-4">
 				<h2 className="text-2xl font-semibold mb-4">Projects</h2>
 				
 				<ProjectTabs
@@ -123,39 +144,6 @@ export const Home = () => {
 						{markdownContent}
 					</ReactMarkdown>
 				</div>
-			</section>
-			
-			<section id="contact" className="w-full max-w-3xl mx-auto text-center px-4 py-12">
-				<h2 className="text-2xl font-semibold mb-4">Contacts</h2>
-				
-				<div className="space-y-4 text-lg text-gray-700 dark:text-gray-300">
-					<p className="flex items-center justify-center gap-2">
-						<HiOutlineMail className="text-xl text-blue-600" />
-						<a href="mailto:ysw991106@gmail.com" className="hover:underline hover:text-blue-800 transition">
-							ysw991106@gmail.com
-						</a>
-					</p>
-					<p className="flex items-center justify-center gap-2">
-						<FaGithub className="text-xl text-gray-800 dark:text-white" />
-						<a href="https://github.com/codearchitect99" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-blue-800 transition">
-							GitHub
-						</a>
-					</p>
-					{/*<p className="flex items-center justify-center gap-2">*/}
-					{/*	<FaLinkedin className="text-xl text-blue-700" />*/}
-					{/*	<a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-blue-800 transition">*/}
-					{/*		LinkedIn*/}
-					{/*	</a>*/}
-					{/*</p>*/}
-					<p className="flex items-center justify-center gap-2">
-						<GoLocation className="text-xl text-red-500" />
-						Jeju, South Korea
-					</p>
-				</div>
-				
-				<p className="mt-6 text-gray-600 dark:text-gray-400">
-					Feel free to reach out if you’d like to collaborate or just say hi!
-				</p>
 			</section>
 		</main>
 	);
