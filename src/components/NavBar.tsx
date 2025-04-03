742f8f30ef78386421c3ceb43acc267e1f337944
codearchitect99 <ysw991106@gmail.com>
2025-04-03 14:21:33 +0900

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
 	return (
-		<nav className="bg-white dark:bg-gray-900 shadow pt-4 border-b border-gray-300">
-			<div className="max-w-7xl mx-auto flex items-center justify-between">
+		<nav className="bg-white dark:bg-gray-900 shadow pt-4 border-b border-gray-300 px-4">
+			<div className="flex">
 				<div className="flex">
 					<NavLink
 						to="/"
 						end
 						className={({ isActive }) =>
-							`px-4 py-2 transition-colors font-medium ${
+							`px-8 py-2 transition-colors font-medium ${
 								isActive
 									? "bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
 									: "text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"
@@ -21,7 +21,7 @@ export const NavBar = () => {
 					<NavLink
 						to="/resume"
 						className={({ isActive }) =>
-							`px-4 py-2 transition-colors font-medium ${
+							`px-8 py-2 transition-colors font-medium ${
 								isActive
 									? "bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
 									: "text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"
@@ -33,7 +33,7 @@ export const NavBar = () => {
 					<NavLink
 						to="/coverletter"
 						className={({ isActive }) =>
-							`px-4 py-2 transition-colors font-medium ${
+							`px-8 py-2 transition-colors font-medium ${
 								isActive
 									? "bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
 									: "text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"
