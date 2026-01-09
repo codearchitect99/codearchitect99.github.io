8e4698bc86398dc0759f7471532864533d96ba1b
codearchitect99 <ysw991106@gmail.com>
2026-01-09 16:35:06 +0900

    remove cover letter link

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Route, HashRouter as Router, Routes} from "react-router-dom";
 import {Resume} from "./pages/resume/Resume.tsx";
 import { Home } from "./pages/home/Home.tsx";
 import { NotFound } from "./pages/notFound/NotFound.tsx";
-import { CoverLetter } from "./pages/coverletter/CoverLetter.tsx";
+// import { CoverLetter } from "./pages/coverletter/CoverLetter.tsx";
 import {NavBar} from "./components/NavBar.tsx";
 
 const App: React.FC = () => {
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -32,18 +32,18 @@ export const NavBar = () => {
 					>
 						Resume
 					</NavLink>
-					<NavLink
-						to="/coverletter"
-						className={({ isActive }) =>
-							`px-8 py-2 transition-colors font-medium ${
-								isActive
-									? "bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
-									: "text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"
-							}`
-						}
-					>
-						Cover Letter
-					</NavLink>
+					{/*<NavLink*/}
+					{/*	to="/coverletter"*/}
+					{/*	className={({ isActive }) =>*/}
+					{/*		`px-8 py-2 transition-colors font-medium ${*/}
+					{/*			isActive*/}
+					{/*				? "bg-gray-200 dark:bg-gray-800 text-black dark:text-white"*/}
+					{/*				: "text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800"*/}
+					{/*		}`*/}
+					{/*	}*/}
+					{/*>*/}
+					{/*	Cover Letter*/}
+					{/*</NavLink>*/}
 				</div>
 			</div>
 		</nav>
